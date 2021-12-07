91f888bbba2b57acd347487b456fa46e690825ff
WenxuanShi <whexy@outlook.com>
2021-12-07 11:53:10 +0800

    init Xcode Project

---
+++ WordSelection/Shared/WordSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSelectionApp.swift
+//  Shared
+//
+//  Created by shiwx on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WordSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
